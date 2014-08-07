ec112cd222355825cbfd57036c60a72990cd2d17
Thomas Ponweiser <thomas.ponweiser@risc-software.at>
2014-08-07 13:03:12 +0200

    removed superfluous call to get_imfghk_list when plotting curv and morb; refs #4

--- src/postw90/kpath.F90
+++ src/postw90/kpath.F90
@@ -270,14 +270,7 @@ contains
              end if
           endif
 
-          if(plot_curv) then
-             call get_imfgh_k_list(kpt,imf_k_list)
-             curv(loop_kpt,1)=sum(imf_k_list(:,1,1))
-             curv(loop_kpt,2)=sum(imf_k_list(:,2,1))
-             curv(loop_kpt,3)=sum(imf_k_list(:,3,1))
-          end if
-
-          if(plot_morb) then
+          if(plot_morb) then 
              call get_imfgh_k_list(kpt,imf_k_list,img_k_list,imh_k_list)
              Morb_k=img_k_list(:,:,1)+imh_k_list(:,:,1)&
                    -2.0_dp*fermi_energy_list(1)*imf_k_list(:,:,1)
@@ -285,6 +278,15 @@ contains
              morb(loop_kpt,1)=sum(Morb_k(:,1))
              morb(loop_kpt,2)=sum(Morb_k(:,2))
              morb(loop_kpt,3)=sum(Morb_k(:,3))
+          end if
+
+          if(plot_curv) then
+             if(.not. plot_morb) then
+                call get_imfgh_k_list(kpt,imf_k_list)
+             end if
+             curv(loop_kpt,1)=sum(imf_k_list(:,1,1))
+             curv(loop_kpt,2)=sum(imf_k_list(:,2,1))
+             curv(loop_kpt,3)=sum(imf_k_list(:,3,1))
           end if
 
        end do !loop_kpt
